8042d2f76e21c6560564d994b6dabbf62d4cf53c
Yosept <yosept.flores@gmail.com>
2019-12-25 11:16:24 -0600

    my_select & my_each_with_index DONE

--- enumerables.rb
+++ enumerables.rb
@@ -6,10 +6,35 @@ module Enumerable
       yield(self[item])
     end
   end
+
+  def my_each_with_index
+    length.times do |item|
+      yield(item, self[item])
+    end
+  end
+
+  def my_select
+    final_arr = []
+    my_each do |item|
+      final_arr << item if yield(item)
+    end
+    final_arr
+  end
 end
 
-exp = [1, 1, 3]
+exp = [1, 2, 12, 7, 4, 34]
 
-exp.my_each do |x|
-  puts "each: #{x}"
+exp.my_each do |value|
+  puts "value: #{value}"
 end
+
+puts '-------------------------------------------------'
+
+exp.my_each_with_index do |index, value|
+  puts "index: #{index}"
+  puts "value: #{value}"
+end
+
+puts '-------------------------------------------------'
+
+puts exp.my_select(&:even?)
